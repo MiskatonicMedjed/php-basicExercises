ca98b5a12ec602e69f47e52382757c3f004e5e52
Francis Boardman <alunomarlonbueno@outlook.com>
2024-06-27 22:47:46 -0300

    improved upon bank3.php, using better clean coding practices and adding functionalities

--- bank3.php
+++ bank3.php
@@ -3,47 +3,77 @@
 class Conta
 {
     private int $saldoEmCentavos;
-    private string $nomeTitular;
-    private string $numeroConta;
 
-    /*public function setSaldoEmCentavos(int $saldoEmCentavos): void
+    public function __construct(
+        public readonly string $nomeTitular,
+        public readonly int $numeroConta,
+        public readonly TipoDeConta $tipoDeConta,
+        int $primeiroDeposito
+    )    
     {
-        $this->saldoEmCentavos = $saldoEmCentavos;
-    }*/
-
-    public function getSaldoEmCentavos(): int
-    {
-        return $this->saldoEmCentavos;
-    }
-
-    /*public function setNomeTitular(string $nomeTitular): void
-    {
-        $this->nomeTitular = $nomeTitular;
-    }*/
-
-    public function getNomeTitular(): string
-    {
-        return $this->nomeTitular;
-    }
-
-    /*public function setNumeroConta(int $numeroConta): void
-    {
-        $this->numeroConta = $numeroConta;
-    }*/
-
-    public function getNumeroConta(): string
-    {
-        return $this->numeroConta;
+        $this->saldoEmCentavos = $primeiroDeposito;
     }
 
     public function saque(float $valorDoSaque): void{
         if($valorDoSaque > 0 && $valorDoSaque < $this->saldoEmCentavos){
-        $this->saldoEmCentavos = $this->saldoEmCentavos - $valorDoSaque;
+        $this->saldoEmCentavos -= $valorDoSaque;
     }
     }
-    public function deposito(float $valorDoDeposito): void{
+    
+    public function deposito (float $valorDoDeposito): void{
         if($valorDoDeposito > 0){
-        $this->saldoEmCentavos = $this->saldoEmCentavos + $valorDoDeposito;
+        $this->saldoEmCentavos += $valorDoDeposito;
     }
+    }
+    
+    public function consultarSaldo (): int //getter
+    {
+        return $this->saldoEmCentavos;
+    }
+}
+
+enum TipoDeConta {
+    case Poupanca;
+    case Corrente;
+    case Investimento;
+    case Universitaria;
+
+    public function temTaxa () : bool
+    {
+        return match ($this) 
+        {
+            TipoDeConta::Corrente, TipoDeConta::Investimento => true,
+            TipoDeConta::Poupanca, TipoDeConta::Universitaria => false,
+        };
+    }
+}
+
+class ContaCorrente extends Conta
+{
+    private const float TAXA_SAQUE = 0.01;
+    private const float TARIFA_MENSAL = 5_00;
+    public function __construct (
+        string $nomeTitular,
+        int $numeroConta,
+        int $primeiroDeposito
+    )
+    {
+    $this->tipoDeConta = TipoDeConta::Corrente;
+    Parent::__construct($nomeTitular, $numeroConta, $tipoDeConta, $primeiroDeposito);
+    }
+
+    #[Override]
+    public function saque(float $valorDoSaque): void
+    {
+        $saqueTotal = $valorDoSaque + $valorDoSaque * self::TAXA_SAQUE;
+        if($valorDoSaque > 0 && $valorDoSaque < $this->saldoEmCentavos)
+        {
+        $this->saldoEmCentavos -= $valorDoSaque; 
+        }
+    }
+
+    public function cobrarTarifa(): void
+    {
+        $this->saldoEmCentavos -= self::TARIFA_MENSAL;
     }
 }
